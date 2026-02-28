42c149b25b8a626d515f8a0b29afb46c66c019bb
Anastasia <nastiasos569@gmail.com>
2026-02-28 13:29:14 +0200

    lb20

---
+++ lb20/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace lb20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.buttonSolve = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введіть коефіцієнти рівняння (a, b, c):";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(273, 126);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(70, 22);
+            this.textBoxA.TabIndex = 1;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(368, 126);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(70, 22);
+            this.textBoxB.TabIndex = 2;
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(463, 126);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(70, 22);
+            this.textBoxC.TabIndex = 3;
+            // 
+            // buttonSolve
+            // 
+            this.buttonSolve.Location = new System.Drawing.Point(324, 172);
+            this.buttonSolve.Name = "buttonSolve";
+            this.buttonSolve.Size = new System.Drawing.Size(150, 24);
+            this.buttonSolve.TabIndex = 4;
+            this.buttonSolve.Text = "Обчислити корені";
+            this.buttonSolve.UseVisualStyleBackColor = true;
+            this.buttonSolve.Click += new System.EventHandler(this.buttonSolve_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 24);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Вийти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(276, 217);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 16);
+            this.labelResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonSolve);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Button buttonSolve;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ lb20/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lb20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSolve_Click(object sender, EventArgs e)
+        {
+            try
+            {
+              
+                double a = double.Parse(textBoxA.Text);
+                double b = double.Parse(textBoxB.Text);
+                double c = double.Parse(textBoxC.Text);
+
+        
+                if (a == 0)
+                    throw new Exception("Коефіцієнт a не може дорівнювати 0!");
+
+                double D = b * b - 4 * a * c;
+
+                if (D > 0)
+                {
+                    double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+
+                    labelResult.Text = $"Два корені: x1 = {x1:F2}, x2 = {x2:F2}";
+                }
+                else if (D == 0)
+                {
+                    double x = -b / (2 * a);
+                    labelResult.Text = $"Один корінь: x = {x:F2}";
+                }
+                else
+                {
+                    labelResult.Text = "Дійсних коренів немає.";
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Помилка! Введіть числові значення.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Обчислення завершено.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
