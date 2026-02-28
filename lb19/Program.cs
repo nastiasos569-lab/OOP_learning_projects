ec6e40bf9e8301eba3bb33501206862d952d96d0
Anastasia <nastiasos569@gmail.com>
2026-02-28 13:00:46 +0200

    lb19

---
+++ lb19/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Введіть рядок (слова через пробіл):");
+        string input = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("Рядок порожній!");
+            return;
+        }
+        string[] words = input.Split (new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        var wordGroups = words
+            .GroupBy(w => w.ToLower())   
+            .Where(g => g.Count() > 1);
+
+        Console.WriteLine("\nОднакові слова та їх кількість:");
+
+        if (!wordGroups.Any())
+        {
+            Console.WriteLine("Однакових слів немає.");
+        }
+        else
+        {
+            foreach (var group in wordGroups)
+            {
+                Console.WriteLine($"Слово \"{group.Key}\" зустрічається {group.Count()} раз(и)");
+            }
+        }
+
+        Console.WriteLine("\nВведіть слово, яке потрібно видалити:");
+        string wordToRemove = Console.ReadLine();
+
+        string[] filteredWords = words
+            .Where(w => !w.Equals(wordToRemove, StringComparison.OrdinalIgnoreCase))
+            .ToArray();
+
+        string result = string.Join(" ", filteredWords);
+
+        Console.WriteLine("\nРядок після видалення:");
+        Console.WriteLine(result);
+    }
+}
---
+++ lb19/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lb19")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lb19")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("3e8d77d1-6441-41f5-ad6d-0b43ed9edc19")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
