db5a708e88a57e2da014ca33b0ac8dfa201f51d1
Anastasia <nastiasos569@gmail.com>
2026-03-04 01:17:09 +0200

    lb19

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace lb19_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBoxRemove = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(360, 311);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Результат";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(340, 352);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 26);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Вийти";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBoxRemove
+            // 
+            this.textBoxRemove.Location = new System.Drawing.Point(340, 268);
+            this.textBoxRemove.Name = "textBoxRemove";
+            this.textBoxRemove.Size = new System.Drawing.Size(107, 26);
+            this.textBoxRemove.TabIndex = 13;
+            this.textBoxRemove.Text = "Видалити";
+            this.textBoxRemove.UseVisualStyleBackColor = true;
+            this.textBoxRemove.Click += new System.EventHandler(this.textBoxRemove_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 26);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Знайти однакові слова";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(320, 231);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 22);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(206, 100);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(388, 22);
+            this.textBoxInput.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(280, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(238, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Введіть слово, яке треба видалити:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(349, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Введіть рядок";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBoxRemove);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button textBoxRemove;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lb19_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string[] words;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBoxInput.Text;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                MessageBox.Show("Рядок порожній!");
+                return;
+            }
+
+            words = input.Split(new char[] { ' ' },
+                                StringSplitOptions.RemoveEmptyEntries);
+
+            var wordGroups = words
+                .GroupBy(w => w.ToLower())
+                .Where(g => g.Count() > 1);
+
+            label3.Text = "";   // очищаємо перед виводом
+
+            if (!wordGroups.Any())
+            {
+                label3.Text = "Однакових слів немає.";
+            }
+            else
+            {
+                foreach (var group in wordGroups)
+                {
+                    label3.Text +=
+                        $"Слово \"{group.Key}\" зустрічається {group.Count()} раз(и)\r\n";
+                }
+            }
+        }
+
+
+        private void textBoxRemove_Click(object sender, EventArgs e)
+        {
+            string input = textBoxInput.Text;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                MessageBox.Show("Рядок порожній!");
+                return;
+            }
+
+            words = input.Split(new char[] { ' ' },
+                                StringSplitOptions.RemoveEmptyEntries);
+            if (words == null)
+            {
+                MessageBox.Show("Спочатку введіть рядок і натисніть 'Знайти'!");
+                return;
+            }
+
+            string wordToRemove = textBoxRemove.Text;
+
+            if (string.IsNullOrWhiteSpace(wordToRemove))
+            {
+                MessageBox.Show("Введіть слово для видалення!");
+                return;
+            }
+
+            string[] filteredWords = words
+                .Where(w => !w.Equals(wordToRemove,
+                        StringComparison.OrdinalIgnoreCase))
+                .ToArray();
+
+            string result = string.Join(" ", filteredWords);
+
+            label3.Text = result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
