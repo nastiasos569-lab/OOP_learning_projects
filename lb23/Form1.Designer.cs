448c69e3b454f5eac9ef977dfee9a59d3acc219f
Anastasia <nastiasos569@gmail.com>
2026-03-06 02:04:43 +0200

    first

---
+++ lb23/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace lb23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "a = ";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(72, 25);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(84, 22);
+            this.textBoxA.TabIndex = 1;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(41, 53);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(115, 23);
+            this.btnDraw.TabIndex = 2;
+            this.btnDraw.Text = "Побудувати";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Вийти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ lb23/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lb23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += Form1_Paint;
+        }
+
+        double a = 1;
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBoxA.Text);
+            this.Invalidate(); 
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            int centerX = this.ClientSize.Width / 2;
+            int centerY = this.ClientSize.Height / 2;
+
+            int scale = 40;
+
+            DrawAxes(g, centerX, centerY);
+
+            Pen pen = new Pen(Color.Blue, 2);
+
+            Point? prev = null;
+
+            for (double t = -5; t <= 5; t += 0.01)
+            {
+                if (Math.Abs(1 + Math.Pow(t, 3)) < 0.0001)
+                    continue;
+
+                double x = (3 * a * t) / (1 + Math.Pow(t, 3));
+                double y = (3 * a * Math.Pow(t, 2)) / (1 + Math.Pow(t, 3));
+
+                int px = centerX + (int)(x * scale);
+                int py = centerY - (int)(y * scale);
+
+                Point p = new Point(px, py);
+
+                if (prev != null)
+                {
+                    g.DrawLine(pen, prev.Value, p);
+                }
+
+                prev = p;
+            }
+        }
+
+        private void DrawAxes(Graphics g, int cx, int cy)
+        {
+            Pen axisPen = new Pen(Color.Black, 2);
+
+            g.DrawLine(axisPen, 0, cy, this.ClientSize.Width, cy);
+            g.DrawLine(axisPen, cx, 0, cx, this.ClientSize.Height);
+
+            Font font = new Font("Arial", 10);
+
+            g.DrawString("X", font, Brushes.Black, this.ClientSize.Width - 20, cy + 5);
+            g.DrawString("Y", font, Brushes.Black, cx + 5, 5);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
