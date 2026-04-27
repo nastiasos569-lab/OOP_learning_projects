560aee7883399a145cd4c00aad49f24aa5a134b8
Anastasia <nastiasos569@gmail.com>
2026-04-28 00:27:06 +0300

    lb27

---
+++ lb27/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace lb27
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxDrives = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxFilterDirs = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxFilterFiles = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxInfo = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.richTextBoxContent = new System.Windows.Forms.RichTextBox();
+            this.pictureBoxPreview = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxDrives
+            // 
+            this.comboBoxDrives.FormattingEnabled = true;
+            this.comboBoxDrives.Location = new System.Drawing.Point(12, 37);
+            this.comboBoxDrives.Name = "comboBoxDrives";
+            this.comboBoxDrives.Size = new System.Drawing.Size(172, 24);
+            this.comboBoxDrives.TabIndex = 0;
+            this.comboBoxDrives.SelectedIndexChanged += new System.EventHandler(this.comboBoxDrives_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cписок дисків (C:, D:\\ і т.д.)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(473, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Назад";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Поточный шлях";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(206, 37);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(250, 22);
+            this.textBoxPath.TabIndex = 4;
+            this.textBoxPath.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxPath_KeyDown_1);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 114);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(121, 310);
+            this.treeView1.TabIndex = 5;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Список папок";
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.ItemHeight = 16;
+            this.listBoxFiles.Location = new System.Drawing.Point(149, 114);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(295, 308);
+            this.listBoxFiles.TabIndex = 7;
+            this.listBoxFiles.SelectedIndexChanged += new System.EventHandler(this.listBoxFiles_SelectedIndexChanged);
+            this.listBoxFiles.DoubleClick += new System.EventHandler(this.listBoxFiles_DoubleClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(146, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Список файлів";
+            // 
+            // textBoxFilterDirs
+            // 
+            this.textBoxFilterDirs.Location = new System.Drawing.Point(461, 142);
+            this.textBoxFilterDirs.Name = "textBoxFilterDirs";
+            this.textBoxFilterDirs.Size = new System.Drawing.Size(146, 22);
+            this.textBoxFilterDirs.TabIndex = 10;
+            this.textBoxFilterDirs.TextChanged += new System.EventHandler(this.textBoxFilterDirs_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(475, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Фільтр папок";
+            // 
+            // textBoxFilterFiles
+            // 
+            this.textBoxFilterFiles.Location = new System.Drawing.Point(461, 206);
+            this.textBoxFilterFiles.Name = "textBoxFilterFiles";
+            this.textBoxFilterFiles.Size = new System.Drawing.Size(146, 22);
+            this.textBoxFilterFiles.TabIndex = 12;
+            this.textBoxFilterFiles.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(475, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Фільтр файлів";
+            // 
+            // textBoxInfo
+            // 
+            this.textBoxInfo.Location = new System.Drawing.Point(461, 277);
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.Size = new System.Drawing.Size(146, 145);
+            this.textBoxInfo.TabIndex = 13;
+            this.textBoxInfo.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(485, 245);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Властивості:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(678, 322);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(104, 16);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Вміст фійлів txt.";
+            // 
+            // richTextBoxContent
+            // 
+            this.richTextBoxContent.Location = new System.Drawing.Point(629, 350);
+            this.richTextBoxContent.Name = "richTextBoxContent";
+            this.richTextBoxContent.Size = new System.Drawing.Size(197, 72);
+            this.richTextBoxContent.TabIndex = 15;
+            this.richTextBoxContent.Text = "";
+            // 
+            // pictureBoxPreview
+            // 
+            this.pictureBoxPreview.Location = new System.Drawing.Point(629, 142);
+            this.pictureBoxPreview.Name = "pictureBoxPreview";
+            this.pictureBoxPreview.Size = new System.Drawing.Size(197, 177);
+            this.pictureBoxPreview.TabIndex = 17;
+            this.pictureBoxPreview.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(661, 114);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(148, 16);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Перегляд зображень:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(753, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 24);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Вийти";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.pictureBoxPreview);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.richTextBoxContent);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxInfo);
+            this.Controls.Add(this.textBoxFilterFiles);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxFilterDirs);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxDrives);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxDrives;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxFilterDirs;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxFilterFiles;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox textBoxInfo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RichTextBox richTextBoxContent;
+        private System.Windows.Forms.PictureBox pictureBoxPreview;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ lb27/Form1.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace lb27
+{
+    public partial class Form1 : Form
+    {
+        string currentPath = "";
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+        string GetFileSecurityInfo(string path)
+        {
+            try
+            {
+                FileSecurity security = File.GetAccessControl(path);
+
+                var owner = security.GetOwner(typeof(NTAccount));
+
+                var rules = security.GetAccessRules(true, true, typeof(NTAccount));
+
+                string result = $"Власник: {owner}\r\n\r\nПрава доступу:\r\n";
+
+                foreach (FileSystemAccessRule rule in rules)
+                {
+                    result += $"{rule.IdentityReference} - {rule.FileSystemRights} - {rule.AccessControlType}\r\n";
+                }
+
+                return result;
+            }
+            catch
+            {
+                return "Немає доступу до атрибутів безпеки";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxDrives.Items.Clear();
+
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                try
+                {
+                    if (drive.IsReady)
+                        comboBoxDrives.Items.Add(drive.Name);
+                }
+                catch { }
+            }
+
+            if (comboBoxDrives.Items.Count > 0)
+                comboBoxDrives.SelectedIndex = 0;
+            else
+                MessageBox.Show("Диски не знайдені!");
+            pictureBoxPreview.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+        void LoadFiles(string path)
+        {
+            listBoxFiles.Items.Clear();
+
+            try
+            {
+                foreach (var file in Directory.GetFiles(path))
+                {
+                    listBoxFiles.Items.Add(file);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Помилка доступу до файлів");
+            }
+        }
+        void LoadData(string path)
+        {
+            try
+            {
+                treeView1.Nodes.Clear();
+                listBoxFiles.Items.Clear();
+                pictureBoxPreview.Image = null;
+                richTextBoxContent.Clear();
+
+                DirectoryInfo rootDir = new DirectoryInfo(path);
+
+                TreeNode rootNode = new TreeNode(rootDir.Name);
+                rootNode.Tag = rootDir.FullName;
+
+                treeView1.Nodes.Add(rootNode);
+
+                LoadSubDirs(rootNode);
+
+                foreach (var file in Directory.GetFiles(path))
+                {
+                    listBoxFiles.Items.Add(file);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Немає доступу");
+            }
+        }
+        void LoadSubDirs(TreeNode node)
+        {
+            try
+            {
+                string path = node.Tag.ToString();
+
+                foreach (var dir in Directory.GetDirectories(path))
+                {
+                    DirectoryInfo di = new DirectoryInfo(dir);
+
+                    TreeNode subNode = new TreeNode(di.Name);
+                    subNode.Tag = di.FullName;
+
+                    node.Nodes.Add(subNode);
+                }
+            }
+            catch { }
+        }
+
+        private void comboBoxDrives_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currentPath = comboBoxDrives.SelectedItem.ToString();
+            textBoxPath.Text = currentPath;
+            LoadData(currentPath);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DirectoryInfo dir = Directory.GetParent(currentPath);
+
+                if (dir != null)
+                {
+                    currentPath = dir.FullName;
+                    textBoxPath.Text = currentPath;
+
+                    LoadData(currentPath);
+                }
+            }
+            catch { }
+        }
+        private void textBoxPath_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string newPath = textBoxPath.Text;
+
+                if (Directory.Exists(newPath))
+                {
+                    currentPath = newPath;
+                    LoadData(currentPath);
+                }
+                else
+                {
+                    MessageBox.Show("Шлях не існує!");
+                }
+            }
+        }
+
+        private void textBoxPath_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string newPath = textBoxPath.Text;
+
+                if (Directory.Exists(newPath))
+                {
+                    currentPath = newPath;
+                    LoadData(currentPath);
+                }
+                else
+                {
+                    MessageBox.Show("Шлях не існує!");
+                }
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string path = e.Node.Tag.ToString();
+
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+
+                textBoxInfo.Text =
+                    $"Назва: {dir.Name}\r\n" +
+                    $"Повний шлях: {dir.FullName}\r\n" +
+                    $"Створено: {dir.CreationTime}\r\n" +
+                    $"Остання зміна: {dir.LastWriteTime}";
+
+                currentPath = path;
+                textBoxPath.Text = currentPath;
+
+                LoadFiles(currentPath);
+            }
+            catch
+            {
+                textBoxInfo.Text = "Помилка";
+            }
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            string path = e.Node.Tag.ToString();
+
+            currentPath = path;
+            textBoxPath.Text = currentPath;
+
+            LoadData(currentPath);
+        }
+
+        private void listBoxFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxFiles.SelectedItem == null) return;
+
+            string filePath = listBoxFiles.SelectedItem.ToString();
+
+            try
+            {
+                FileInfo file = new FileInfo(filePath);
+
+                textBoxInfo.Text =
+                    $"Ім'я: {file.Name}\r\n" +
+                    $"Шлях: {file.FullName}\r\n" +
+                    $"Розмір: {file.Length} байт\r\n" +
+                    $"Тип: {file.Extension}\r\n" +
+                    $"Створено: {file.CreationTime}";
+
+                textBoxInfo.Text += GetFileSecurityInfo(file.FullName);
+
+                string ext = file.Extension.ToLower();
+
+                if (ext == ".jpg" || ext == ".png" || ext == ".bmp")
+                {
+                    pictureBoxPreview.Image = null;
+
+                    using (var bmpTemp = new Bitmap(filePath))
+                    {
+                        pictureBoxPreview.Image = new Bitmap(bmpTemp);
+                    }
+                    richTextBoxContent.Clear();
+                }
+                else if (ext == ".txt")
+                {
+                    pictureBoxPreview.Image = null;
+
+                    try
+                    {
+                        richTextBoxContent.Text = File.ReadAllText(filePath, Encoding.UTF8);
+                    }
+                    catch
+                    {
+                        richTextBoxContent.Text = File.ReadAllText(filePath, Encoding.Default);
+                    }
+                }
+                else
+                {
+                    pictureBoxPreview.Image = null;
+                    richTextBoxContent.Clear();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Помилка відкриття файлу");
+            }
+        }
+
+        private void listBoxFiles_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBoxFiles.SelectedItem == null) return;
+
+            try
+            {
+                string filePath = listBoxFiles.SelectedItem.ToString();
+
+                System.Diagnostics.Process.Start(filePath);
+            }
+            catch
+            {
+                MessageBox.Show("Не вдалося відкрити файл");
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentPath)) return;
+
+            try
+            {
+                listBoxFiles.Items.Clear();
+
+                string filter = textBoxFilterFiles.Text.ToLower();
+
+                var files = Directory.GetFiles(currentPath);
+
+                foreach (var file in files)
+                {
+                    if (Path.GetFileName(file).ToLower().Contains(filter))
+                    {
+                        listBoxFiles.Items.Add(file);
+                    }
+                }
+            }
+            catch { }
+        }
+
+        void FilterDirs(string path, string filter)
+        {
+            treeView1.Nodes.Clear();
+
+            try
+            {
+                DirectoryInfo rootDir = new DirectoryInfo(path);
+
+                TreeNode rootNode = new TreeNode(rootDir.Name);
+                rootNode.Tag = rootDir.FullName;
+
+                treeView1.Nodes.Add(rootNode);
+
+                foreach (var dir in Directory.GetDirectories(path))
+                {
+                    if (Path.GetFileName(dir).ToLower().Contains(filter.ToLower()))
+                    {
+                        TreeNode node = new TreeNode(Path.GetFileName(dir));
+                        node.Tag = dir;
+
+                        rootNode.Nodes.Add(node);
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void textBoxFilterDirs_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentPath)) return;
+
+            string filter = textBoxFilterDirs.Text;
+
+            if (string.IsNullOrEmpty(filter))
+                LoadData(currentPath);
+            else
+                FilterDirs(currentPath, filter);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+}
